c2166aab9671f0ba134872fca4a3b6ed1ae44859
Petch Tanathip <nm3207petchza@gmail.com>
2023-07-09 16:01:41 +0700

    passed

--- chap2item2.py
+++ chap2item2.py
@@ -1,39 +1,27 @@
+pi = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328238644709384
 class Spherical:
 
     def __init__(self,r):
 
         self.radius = r
-        self.pi = calculate_pi(1000000)
-
+        
     def changeR(self,Radius):
 
         self.radius = Radius
 
     def findVolume(self):
 
-        vol = (4/3)*self.pi*(self.radius**3)
+        vol = (4/3)*pi*(self.radius**3)
         return vol
 
     def findArea(self):
 
-        area = 4*self.pi*(self.radius**2)
+        area = 4*pi*(self.radius**2)
         return area
 
     def __str__(self):
 
         return "Radius ={} Volumn = {} Area = {}".format(self.radius, self.findVolume(), self.findArea())
-
-def calculate_pi(iterations):
-    pi = 0.0
-    sign = 1.0
-
-    for i in range(iterations):
-        term = sign / (2 * i + 1)
-        pi += term
-        sign *= -1
-
-    pi *= 4.0
-    return pi
     
 
 r1, r2 = input("Enter R : ").split()
