dc90cae3cbe4d1a4164178b4498f7e8d223dda39
Petch Tanathip <nm3207petchza@gmail.com>
2023-07-11 00:50:11 +0700

    not passed

--- Chapter12/chap12item2.py
+++ Chapter12/chap12item2.py
@@ -9,16 +9,17 @@ print(id_score)
 print(that_std_id)
 
 id_score_dict = {}
-for i in range(0, len(id_score) - 1, 2):
+for i in range(0, len(id_score), 2):
     id_score_dict[id_score[i]] = float(id_score[i + 1])
 
 print(id_score_dict)
 
 found = False
-for i in range(len(id_score)):
+for i in range(0, len(id_score), 2):
     if id_score[i] == that_std_id:
-        print(i//2 + 1)
+        print((i // 2) + 1)
         found = True
+        break
 
 if not found:
     print("Not Found")
