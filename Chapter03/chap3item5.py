bd7b236a3ff0280753528117f13245e172c0e9c6
Petch Tanathip <nm3207petchza@gmail.com>
2023-07-18 02:56:58 +0700

    chap3 item5 in progress

---
+++ Chapter03/chap3item5.py
@@ -0,0 +1,92 @@
+# โดนผลกระทบจากเห็ดพิษ 
+# เลขคี่ ความสูงเพิ่ม 2 เมตร
+# เลขคู่ ความสูงลด 1 เมตร
+
+class Stack:
+    def __init__(self, list=None):
+        if list == None:
+            self.items = []
+        else:
+            self.items = list
+
+    def push(self, value):
+        self.items.append(value)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[-1]
+    
+    def isEmpty(self):
+        return len(self.items) == 0
+    
+    def size(self):
+        return len(self.items)
+
+def check_tree_or_look(input):
+    if len(input)==1:
+        return input
+    else:
+        height = [e for e in input.split(' ')][1]
+        return int(height)
+
+S = Stack()
+inp = input('Enter Input : ').split(',')
+
+for i in inp:
+    data = check_tree_or_look(i)
+
+    if data == 'B':
+        temp = Stack()
+        if S.isEmpty():
+            num_of_trees = 0
+        else:
+            max = S.pop()
+            temp.push(max)
+            num_of_trees = 1
+
+        while (not S.isEmpty()):
+            value = S.pop()
+            temp.push(value)
+
+            if max < value:
+                max = value
+                num_of_trees += 1
+        
+        while (not temp.isEmpty()):
+            S.push(temp.pop())
+            
+        print(num_of_trees)
+
+    elif data == 'S':
+        temp2 = Stack()
+
+        if S.isEmpty():
+            num_of_trees = 0
+        else:
+            max2 = S.pop()
+            if max2%2 == 0:
+                new_max = max2-1
+                temp2.push(new_max)
+            else:
+                new_max = max2+2
+                temp2.push(new_max)
+
+        while (not S.isEmpty()):
+            value2 = S.pop()
+            if value2%2 == 0:
+                new_value = max2-1
+                temp2.push(new_value)
+            else:
+                new_value = max2+2
+                temp2.push(new_value)
+
+            if new_max < new_value:
+                new_max = new_value
+        
+        while (not temp2.isEmpty()):
+            S.push(temp2.pop())
+
+    else:
+        S.push(data)
