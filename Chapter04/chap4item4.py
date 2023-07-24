ae00a97b36901b7c03628f1da24c083c51562ad6
Petch Tanathip <nm3207petchza@gmail.com>
2023-07-25 01:19:13 +0700

    : chap4 item4 passed

--- Chapter04/chap4item4.py
+++ Chapter04/chap4item4.py
@@ -34,22 +34,46 @@ class Queue:
     def size(self):
         return len(self.items)
     
-class Customer:
-    def __init__(self, si, pi):
-        self.si = si
-        self.pi = pi
-        
-print(' ***Cafe***')
-log = input('input : ').split('/')
 
-cafe = Queue()
-b1 = Queue()
-b2 = Queue()
-time = 0
+# prof Nam's solution
+def cafe(line):
+    command = Queue(line)
+    barista1 = 0
+    barista2 = 0
+    customer_who_wasted_their_time_the_most = 0
+    max_wait_time = 0
+    finish_list = []
+    while not command.isEmpty():
+        in_time, coffee_time, customer_number = command.dequeue()
+        if barista1 > barista2 and in_time < barista1:
+            barista1, barista2 = barista2, barista1
+        current_customer_wait_time = 0
+        if in_time < barista1:
+            current_customer_wait_time = barista1 - in_time
+        else:
+            barista1 = in_time
 
-for cs in log:
-    si = int(cs.split(',')[0])
-    pi = int(cs.split(',')[1])
+        # print(customer_number, current_customer_wait_time)
+        if current_customer_wait_time > max_wait_time:
+            max_wait_time = current_customer_wait_time
+            customer_who_wasted_their_time_the_most = customer_number
 
-    cafe.enqueue(Customer(si, pi))
+        barista1 += coffee_time
 
+        finish_list.append([barista1, customer_number])
+
+    finish_list.sort()
+    [print(f'Time {e[0]} customer {e[1]} get coffee') for e in finish_list]
+
+    if max_wait_time == 0:
+        print(f'No waiting')
+    else:
+        print(f'The customer who waited the longest is : {customer_who_wasted_their_time_the_most}')
+        print(f'The customer waited for {max_wait_time} minutes')
+
+
+print(" ***Cafe***")
+inp = [[int(c) for c in e.split(',')] + [i + 1] for i, e in enumerate(input("Log : ").split('/'))]
+
+# inp.sort(key=lambda x: x[0])
+cafe(inp)
