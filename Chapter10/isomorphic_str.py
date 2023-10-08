5868ab57b4cbfd902ff1263a8bddbb1548b01ce9
Petch Tanathip <nm3207petchza@gmail.com>
2023-10-08 15:49:48 +0700

    <add> : Isomorphic string

---
+++ Chapter10/isomorphic_str.py
@@ -0,0 +1,16 @@
+def isomorphic(string1, string2):
+    if(len(string1) != len(string2)):
+        return False
+    x = [string1.count(char1) for char1 in string1]
+    y = [string2.count(char1) for char1 in string2]
+    return x == y
+
+
+string_input = input("Enter str1,str2: ").split(',')
+string1 = string_input[0].strip()
+string2 = string_input[1].strip()
+
+if isomorphic(string1, string2):
+    print(f"{string1} and {string2} are Isomorphic")
+else:
+    print(f"{string1} and {string2} are not Isomorphic")
